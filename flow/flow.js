c07ba96509e52052bab403773642d085925f0584
zhengchen1996 <1226965295@qq.com>
2020-03-09 15:30:48 +0800

    Initial commit

---
+++ flow/flow.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+    call: {
+        baidu_lxb: require('../flow/call/baidu_lxb')
+        // baidu_shangqiao: require('../flow/call/baidu_shangqiao')
+    },
+    comment: {
+        baidu_shangqiao: require('../flow/comment/baidu_shangqiao')
+    },
+    sms: {
+        // TODO
+    }
+
+}
